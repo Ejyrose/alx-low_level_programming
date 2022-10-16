41767f385e45ac792b62ab8673600d7717b517a2
ejyrose <jyko2017@gmail.com>
2022-10-16 13:52:20 -0700

    please

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main-program entry point.
  * Return:0 success, non zero error.
@@ -7,7 +8,7 @@ int main(void)
 {
 	int num;
 
-	for (num = '48'; num <= '58'; num++)
+	for (num = 48; num <= 58; num++)
 	{
 		putchar(num);
 		if (num != 57)
