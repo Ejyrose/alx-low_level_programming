b8ab15593c85d52f11d90201aa4f6c523089add3
ejyrose <jyko2017@gmail.com>
2022-10-19 11:35:15 -0700

    again

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - print the alphabet 10 times in lowercase add a new line
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
