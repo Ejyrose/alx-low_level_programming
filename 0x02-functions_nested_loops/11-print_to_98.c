ec436a200cbfcdbe8a893752ce6e873d1c59ef36
ejyrose <jyko2017@gmail.com>
2022-10-19 21:13:00 -0700

    so cool

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * print_to_98 - Print all natural numbers from input to 98,
- * must be separated by a comma followed by a space
- * @n: The number to begin counting at
+ *        must be separated by a comma followed by a space.
+ * @n: The number to begin counting at.
  */
 void print_to_98(int n)
 {
