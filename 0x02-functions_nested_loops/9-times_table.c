2d58120a41e743e5e5cdfd91f8f9d68acf1b2651
ejyrose <jyko2017@gmail.com>
2022-10-19 20:14:59 -0700

    soon

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 /**
  * times_table - Prints the 9 times table, starting with 0
  */
-void times_table(void)
+void time_table(void)
 {
 	int num, mult, prod;
 
@@ -18,12 +18,12 @@ void times_table(void)
 
 			prod = num * mult;
 
-			if (prod <= 9)
+			if (pro <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
-		_putchar('\n');
+		_putchar('\n);
 	}
 }
