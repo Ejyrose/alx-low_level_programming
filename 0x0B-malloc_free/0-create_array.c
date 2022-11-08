424aa55ef948aef50cc95e95b5a83bf37e4d1b96
ejyrose <jyko2017@gmail.com>
2022-11-08 10:17:57 -0800

    allover

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,16 +13,16 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
-	
+
 	if (size == 0)
 		return (NULL);
-	
+
 	array = (char *)malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < size; i++)
 		array[i] = c;
-	
+
 	return (array);
 }
