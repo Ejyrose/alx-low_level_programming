5de0bedcf323f2c13af20ffbd1572de0e729dc9b
ejyrose <jyko2017@gmail.com>
2022-11-09 11:52:09 -0800

    again

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in  reverse
- * @s: string to be printed
+ * print_rev - this function return the legth of a string
+ * @s: value
  */
 void print_rev(char *s)
 {
-	int i = 0;
+	char n;
+	int i =  0;
 
-	while (s[i] != '\0')
+	while (n != '\0')
+	{
+		n = s[i];
 		i++;
-	for (i = i - 1; i >= 0; i--)
+	}
+	i--;
+	while (i >= 1)
+	{
+		i--;
 		_putchar(s[i]);
-
+	}
 	_putchar('\n');
 }
